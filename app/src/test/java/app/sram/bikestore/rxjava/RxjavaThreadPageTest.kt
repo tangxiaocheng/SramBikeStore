33095915864fa8abd403a710e8b31af37118b7c6
Randy <tangdengcheng@gmail.com>
2021-02-09 22:56:05 -0800

    Add defer demo

--- app/src/test/java/app/sram/bikestore/rxjava/RxjavaThreadPageTest.kt
+++ app/src/test/java/app/sram/bikestore/rxjava/RxjavaThreadPageTest.kt
@@ -3,12 +3,12 @@ package app.sram.bikestore.rxjava
 import com.google.common.truth.Truth
 import io.reactivex.Observable
 import io.reactivex.Scheduler
-import io.reactivex.Single
 import io.reactivex.schedulers.Schedulers
+import io.reactivex.subjects.ReplaySubject
 import org.junit.After
 import org.junit.Before
 import org.junit.Test
-import java.util.concurrent.TimeUnit
+
 
 /**
  * A pure RxJava prototype that is aim to mock the process of fetching all the pages of the Google map place API result.
@@ -28,17 +28,17 @@ class RxjavaThreadPageTest {
     *
     * */
 
-    internal data class PageResult(val pageList: List<String>, val pageToken: String?)
+    internal data class PageResult(val pageList: List<String>, val pageToken: String)
 
-    private val startToken = "startToken"
-    private val endToken = "endToken"
+    private val START_TOKEN = "startToken"
+    private val END_TOKEN = "endToken"
 
     private val dataSource = mapOf(
-        startToken to PageResult(listOf("list1_1", "list1_2", "list1_3"), "tokenA"),
+        START_TOKEN to PageResult(listOf("list1_1", "list1_2", "list1_3"), "tokenA"),
         "tokenA" to PageResult(listOf("list2_1", "list2_2", "list2_3"), "tokenB"),
         "tokenB" to PageResult(listOf("list3_1", "list3_2", "list3_3"), "tokenC"),
         "tokenC" to PageResult(listOf("list4_1", "list4_2", "list4_3"), "tokenD"),
-        "tokenD" to PageResult(listOf("list5_1", "list5_2", "list5_3"), endToken)
+        "tokenD" to PageResult(listOf("list5_1", "list5_2", "list5_3"), END_TOKEN)
     )
 
 
@@ -54,22 +54,26 @@ class RxjavaThreadPageTest {
 
     @Test
     fun testTokenPage() {
-        val resList = mutableListOf<String>()
-        fetchPageByToken(startToken, resList)
-        Truth.assertThat(resList.size).isEqualTo(3 * 5)
-    }
 
-    private fun fetchPageByToken(token: String, list: MutableList<String>) {
-        Observable.just(token)
-            .flatMap { getPage(it, list) }
-            .filter { it != endToken }
-            .subscribe { fetchPageByToken(it!!, list) }
-    }
 
-    private fun getPage(token: String, list: MutableList<String>): Observable<String> {
-        print("$token -> ")
-        list.addAll(dataSource[token]!!.pageList)
-        return Observable.just(dataSource[token]!!.pageToken)
+        val defer = Observable.defer {
+            val replaySubject = ReplaySubject.create<String>(10)
+            replaySubject.concatMap { pageToken: String ->
+                if (pageToken == END_TOKEN) {
+                    return@concatMap Observable.empty<List<String>>().doOnComplete {
+                        replaySubject.onComplete()
+                    }
+                } else {
+                    return@concatMap Observable.just(dataSource[pageToken])
+                        .doOnNext { replaySubject.onNext(it!!.pageToken) }
+                }
+            }
+            replaySubject.onNext(START_TOKEN)
+            replaySubject
+        }
+        val test = defer.test()
+//        Truth.assertThat(test.valueCount()).isEqualTo(10)
+        Truth.assertThat(test.values()).isNotEmpty()
     }
 
 
