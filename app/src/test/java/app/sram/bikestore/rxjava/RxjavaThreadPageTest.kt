ecce0b8e03fcaa781e33ff3320608dca1eedf901
Randy <tangdengcheng@gmail.com>
2021-02-10 11:39:44 -0800

    Get List of PageResult by ReplaySubject

--- app/src/test/java/app/sram/bikestore/rxjava/RxjavaThreadPageTest.kt
+++ app/src/test/java/app/sram/bikestore/rxjava/RxjavaThreadPageTest.kt
@@ -3,6 +3,7 @@ package app.sram.bikestore.rxjava
 import com.google.common.truth.Truth
 import io.reactivex.Observable
 import io.reactivex.Scheduler
+import io.reactivex.Single
 import io.reactivex.schedulers.Schedulers
 import io.reactivex.subjects.ReplaySubject
 import org.junit.After
@@ -28,18 +29,6 @@ class RxjavaThreadPageTest {
     *
     * */
 
-    internal data class PageResult(val pageList: List<String>, val pageToken: String)
-
-    private val START_TOKEN = "startToken"
-    private val END_TOKEN = "endToken"
-
-    private val dataSource = mapOf(
-        START_TOKEN to PageResult(listOf("list1_1", "list1_2", "list1_3"), "tokenA"),
-        "tokenA" to PageResult(listOf("list2_1", "list2_2", "list2_3"), "tokenB"),
-        "tokenB" to PageResult(listOf("list3_1", "list3_2", "list3_3"), "tokenC"),
-        "tokenC" to PageResult(listOf("list4_1", "list4_2", "list4_3"), "tokenD"),
-        "tokenD" to PageResult(listOf("list5_1", "list5_2", "list5_3"), END_TOKEN)
-    )
 
 
     private lateinit var io: Scheduler
@@ -50,30 +39,6 @@ class RxjavaThreadPageTest {
     fun setUp() {
         io = Schedulers.io()
         ui = Schedulers.trampoline()
-    }
-
-    @Test
-    fun testTokenPage() {
-
-
-        val defer = Observable.defer {
-            val replaySubject = ReplaySubject.create<String>(10)
-            replaySubject.concatMap { pageToken: String ->
-                if (pageToken == END_TOKEN) {
-                    return@concatMap Observable.empty<List<String>>().doOnComplete {
-                        replaySubject.onComplete()
-                    }
-                } else {
-                    return@concatMap Observable.just(dataSource[pageToken])
-                        .doOnNext { replaySubject.onNext(it!!.pageToken) }
-                }
-            }
-            replaySubject.onNext(START_TOKEN)
-            replaySubject
-        }
-        val test = defer.test()
-//        Truth.assertThat(test.valueCount()).isEqualTo(10)
-        Truth.assertThat(test.values()).isNotEmpty()
     }
 
 
